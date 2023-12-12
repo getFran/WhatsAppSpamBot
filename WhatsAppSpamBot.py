8918b2e2ff47e21971e484f04860108de0710b7c
Fran <58342739+getFran@users.noreply.github.com>
2023-12-12 14:09:28 +0100

    Add files via upload

---
+++ WhatsAppSpamBot.py
@@ -0,0 +1,19 @@
+import pyautogui, webbrowser
+from time import sleep
+
+# Include your country code and no spaces
+phone = '+34651413766'
+
+# Open whatsapp web
+webbrowser.open(f'https://web.whatsapp.com/send?phone={phone}')
+sleep(15)
+
+# Open a simple file
+with open('text.txt','r') as file:
+
+    for line in file:
+        # Write the lines on page opened
+        pyautogui.typewrite(line)
+
+        # Press enter (send message)
+        pyautogui.press("enter")
